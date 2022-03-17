c49a0301ec8023612390a37ecea5eb32876dcf38
frazer wilson <frazerwilson145@gmail.com>
2022-03-17 21:07:49 +0000

    theme, shift to left

--- themes.js
+++ themes.js
@@ -2,12 +2,14 @@ const darkThemes = [
     {main:'#07065A', contrast: '#ff84d1'},
     {main:'#410202', contrast: '#faf9cb'},
     {main:'#042905', contrast: '#fdfff2'},
+    {main:'#200a3b', contrast: '#c1a6f4'},
 ]
 
 const lightThemes = [
     {main:'#d9ffa8', contrast:'#211e1e'},
     {main:'#ffc2f6', contrast:'#0c1f5e'},
     {main:'#ffcfc2', contrast:'#07234c'},
+    {main:'#fff6bc', contrast:'#681ad4'},
 ]
 
 const getTheme = () => {
